ce42d549d992609ec30cc98707927b2b66bdc712
Srimal Fernando <dilankasrimal23@gmail.com>
2023-06-11 23:52:10 +0530

    create and index models

--- Pages/Clients/Create.cshtml.cs
+++ Pages/Clients/Create.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
 namespace nma_graphics.Pages.Clients
 {
@@ -18,14 +19,40 @@ namespace nma_graphics.Pages.Clients
             clientInfo.email = Request.Form["email"];
             clientInfo.phone = Request.Form["phone"];
             clientInfo.address = Request.Form["address"];
-            if (clientInfo.name.Length == 0 || clientInfo.email.Length == 0 ||     
-                clientInfo.phone.Length == 0 || clientInfo.address.Length == 0)
+            if (clientInfo.name.Length == 0 || clientInfo.email.Length == 0 || clientInfo.phone.Length == 0 || clientInfo.address.Length == 0)
             {
                 errorMessage = "All fields are required";
                 return;
             }
 
             // save the new client into the database
+            try
+            {
+                String conectionString = "Data Source=.\\sqlexpress;Initial Catalog=Kinetico;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(conectionString))
+                {
+                    connection.Open();
+                    String sql = "INSERT INTO clients " +
+                        "(name, email, phone, address) VALUES " +
+                        "(@name, @email, @phone, @address);";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", clientInfo.name);
+                        command.Parameters.AddWithValue ("@email", clientInfo.email);
+                        command.Parameters.AddWithValue("@phone", clientInfo.phone);
+                        command.Parameters.AddWithValue("@address", clientInfo.address);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
 
             clientInfo.name = ""; clientInfo.email = ""; clientInfo.phone = ""; clientInfo.address = "";
             successMessage = "New Client Added Successfully";
