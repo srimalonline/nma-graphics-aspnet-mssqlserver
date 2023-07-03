16c9ca9ce28a05011c1ecfc7821f3b4f7bc139f9
Srimal Fernando <dilankasrimal23@gmail.com>
2023-07-03 23:37:25 +0530

    creating create order

---
+++ Pages/Orders/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nma_graphics.Pages.Orders
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
