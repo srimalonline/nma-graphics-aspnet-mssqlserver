42adb49a6e9f39d44e8dc90ebf74b81bef7c9a07
Srimal Fernando <dilankasrimal23@gmail.com>
2023-07-10 23:30:47 +0530

    order index

---
+++ Pages/Orders/OrderIndex.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nma_graphics.Pages.Orders
+{
+    public class OrderIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
